758222766e280c33594e445453d683057a4ca159
dontech09 <donnjondanmakal@gmail.com>
2021-09-04 19:13:56 +0530

    Home

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,5 +1,18 @@
+import { BsSearch } from "react-icons/bs";
 const Home = () => {
-  return <h1>Hi</h1>;
+  return (
+    <div className="holder">
+      <div className="holder-col1">
+        <h1>GitTree</h1>
+        <h2>Enter Github username of you</h2>
+        <div className="input">
+          <input type="text" placeholder="Username" />
+          <BsSearch size="2em" color="#fff" className="floatIcn" />
+        </div>
+      </div>
+      <div className="holder-col2">s</div>
+    </div>
+  );
 };
 
 export default Home;
